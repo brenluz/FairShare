89670b38b0b34a8238b3b16c3fc901f8486445a4
Breno <brenluz@outlook.com>
2026-05-27 23:08:02 -0300

    User repository and service

---
+++ src/main/java/com/brenluz/fairshare/domain/user/UserService.java
@@ -0,0 +1,23 @@
+package com.brenluz.fairshare.domain.user;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Transactional
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
